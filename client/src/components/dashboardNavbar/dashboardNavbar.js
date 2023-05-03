07da31da2e66ed1c11ac4275cddd5cced5f675db
Akshith Kandivanam <akshithkandivanam@gmail.com>
2023-05-02 22:48:03 -0400

    Incorporated log-out cooperation with backend

--- client/src/components/dashboardNavbar/dashboardNavbar.js
+++ client/src/components/dashboardNavbar/dashboardNavbar.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 import '../../styles/dashboardNavbar/dashboardNavbar.css'
 import logo from '../../assets/REPETIPRO.png';
 
 function DashboardNavbar() {
+
+  async function handleLogout() {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/logout`, {
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`,
+          'Cache-Control': 'no-cache',
+        }
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error.response.data);
+      alert(error.response.data);
+    }
+  }
+
+  function getAuthToken() {
+    const authToken = document.cookie.split('; ').find(row => row.startsWith('authToken=')).split('=')[1];
+    return authToken;
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
         <a href='/home'>
-        <img src={logo} alt="Repetipro Logo" className="navbar-logo" />
+          <img src={logo} alt="Repetipro Logo" className="navbar-logo" />
         </a>
         <a href='/home'>
-        <h1 className="navbar-brand">RepetiPro 🧑‍💻</h1>
+          <h1 className="navbar-brand">RepetiPro 🧑‍💻</h1>
         </a>
       </div>
       <ul className="navbar-right">
@@ -21,7 +43,7 @@ function DashboardNavbar() {
           <a className="nav-link" href="/calendar">Calendar</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/logout">Log out</a>
+          <a className="nav-link" href="/" onClick={handleLogout}>Log out</a>
         </li>
       </ul>
     </nav>
