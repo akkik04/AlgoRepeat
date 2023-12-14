4678c31f42c378026323fba1d5eea59e926ad9c4
Akshith Kandivanam <akshithkandivanam@gmail.com>
2023-12-14 01:13:13 -0500

    Sub-pages for entrynavbar

---
+++ client/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div>
+      <h2>Sign Up Page</h2>
+      {/* Add content for the Sign Up page */}
+    </div>
+  );
+};
+
+export default SignUp;
