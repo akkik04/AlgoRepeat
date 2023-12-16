5ebba932fd63a938dbaf801756d2ae99b8cdbc35
Akshith Kandivanam <akshithkandivanam@gmail.com>
2023-12-15 23:39:51 -0500

    Add useLogIn hook and update LogIn page

---
+++ client/src/hooks/useLogIn.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import axios from 'axios';
+interface ErrorResponse {
+    response: {
+      data: {
+        error: string;
+      };
+    };
+  }
+  
+  const useLogIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState<string | null>(null);
+    const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  
+    const login = async (email: string, password: string) => {
+      try {
+        const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, {
+          email,
+          password,
+        });
+  
+        if (response.status === 200) {
+          setSuccessMessage(response.data.message);
+        } else {
+          setError(response.data.error);
+        }
+      } catch (error) {
+        console.error('Error occurred during login:', error);
+  
+        // Check if the error response is available and contains an error message
+        if (axios.isAxiosError(error) && error.response && error.response.data && error.response.data.error) {
+          const axiosError = error as ErrorResponse;
+          setError(axiosError.response.data.error);
+        } else {
+          setError('Error occurred. Please try again.');
+        }
+      }
+    };
+  
+    return { email, setEmail, password, setPassword, error, successMessage, login };
+  };
+  
+  export default useLogIn;
--- client/src/pages/LogIn/LogIn.tsx
+++ client/src/pages/LogIn/LogIn.tsx
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useLogIn from '../../hooks/useLogIn'; // Adjust the import path
 
-const Login = () => {
+const LogIn = () => {
+  const { email, setEmail, password, setPassword, successMessage, error, login } = useLogIn();
+  const primaryColor = '#000'; // Change this to the actual primary color
+
+  const handleLogIn = async () => {
+    await login(email, password);
+    setEmail('');
+    setPassword('');
+  };
+
+  useEffect(() => {
+    if (successMessage) {
+      alert(successMessage);
+    }
+  }, [successMessage]);
+
   return (
-    <div>
-      <h2>Login Page</h2>
-      
+    <div className="flex justify-center items-center h-full">
+      <div className="p-8 text-center flex flex-col gap-10">
+        <h2 className="text-3xl font-bold mb-4" style={{ color: primaryColor }}>
+          Log In
+        </h2>
+        <div className="login-box bg-gray-100 rounded-lg p-8 max-w-md mx-auto">
+          <label className="block text-lg font-semibold mb-2" style={{ color: primaryColor }}>
+            Email
+          </label>
+          <input
+            type="email"
+            className="border rounded p-3 mb-4 w-full"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label className="block text-lg font-semibold mb-2" style={{ color: primaryColor }}>
+            Password
+          </label>
+          <input
+            type="password"
+            className="border rounded p-3 mb-4 w-full"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="bg-blue-500 text-white rounded py-2 px-4 hover:bg-blue-700 transition duration-300"
+            onClick={handleLogIn}
+          >
+            Log In
+          </button>
+          {error && <p style={{ color: 'red', marginTop: '10px' }}>{error}</p>}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Login;
+export default LogIn;
